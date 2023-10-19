62f0cf6a620233a12d713bc13a9dc74bc3b481f7
Pradarss <verma0025adarsh@gmail.com>
2023-10-19 14:39:12 +0530

    ..

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,11 @@ import Navbar from "./Navbar";
 
 function Home(){
     return(
-<<<<<<< HEAD
-        <h1> Home</h1>
-        
-=======
         <HomeArea >
             <Navbar />
         </HomeArea>
 
 
->>>>>>> 9f34ed3f789a81bf01436378d4f476f053a2d20e
     )
 }
 
