4b1f9a87eb770d6b3335bfd1dcb73503958346ec
Pradarss <verma0025adarsh@gmail.com>
2023-10-19 20:59:25 +0530

    ..

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -2,13 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from "react";
 import Navbar from './Navbar';
 import Footer from "./Footer";
-<<<<<<< HEAD:src/components/main/App.jsx
-import Home from "./LandingPage/Home";
+import Home from "../LandingPage/Home/Home";
 import Login from "../LoginPage/Login";
-=======
-import Home from "./LandingPage/Home/Home";
-import Login from "./LoginPage/Login";
->>>>>>> f345c4da23f041e3855d16901a8f69d36a091e44:src/components/App.jsx
 // import Dashboard from "./Dashboard";
 
 function App(){
