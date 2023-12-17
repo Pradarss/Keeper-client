c0896d297e3ddb99a9eecaec3beda20c7c361f40
Pradarss <verma0025adarsh@gmail.com>
2023-12-18 00:08:38 +0530

    ..

--- src/components/Dashboards/Dashboard.jsx
+++ src/components/Dashboards/Dashboard.jsx
@@ -5,7 +5,7 @@ import UserMenu from "./UserMenu/UserMenu";
 import ToDo from "./Lists/ToDo";
 import Doing from "./Lists/Doing";
 import Done from "./Lists/Done";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getCurrentTime } from "./Lists/time";
 import CreateNoteArea from "./Lists/CreateNoteArea";
 
@@ -59,14 +59,22 @@ function Employee(){
             .then(function(response){
                 return response.json();
             })
-            .then(function(savedTask){
-                setTodo((prevTasks) => [
-                    ...prevTasks,
-                    savedTask
-                ]);
-            })
-            console.log(newTask);
     }
+
+    useEffect(() => {
+        // Fetch tasks from the backend API when the component mounts
+        fetch("http://localhost:5000/dashboard")
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            // setTodo((prevTasks) => [
+            //     ...prevTasks,
+            //     data
+            // ]);
+            setTodo(data);
+          })
+          .catch(error => console.error("Error fetching tasks:", error));
+      }, []);
 
     function deleteTask(content){
         setDone((prevTasks) =>
@@ -82,7 +90,7 @@ function Employee(){
             <Grid item xs={2}>  
 
                 <UserArea>
-                    <UserMenu />
+                    <UserMenu user={user}/>
                 </UserArea>
 
             </Grid>
